3c274d9a28ced7ad028bcda84539a2df1dc79471
Jonas Andersson <jonas@andersson.co>
2022-05-29 17:44:28 +0200

    fix csv

--- utilitites/CSVCreator.ts
+++ utilitites/CSVCreator.ts
@@ -15,188 +15,188 @@ const Fields = {
   firstName: {
     slug: "firstName",
     title: "Vorname",
-    head: "Vorname",
-    toText: (tn: TeilnehmerIn) => tn.firstName,
+    head: '"Vorname"',
+    toText: (tn: TeilnehmerIn) => `"${tn.firstName}"`,
   },
   lastName: {
     slug: "lastName",
     title: "Nachname",
-    head: "Nachname",
-    toText: (tn: TeilnehmerIn) => tn.lastName,
+    head: '"Nachname"',
+    toText: (tn: TeilnehmerIn) => `"${tn.lastName}"`,
   },
   orderId: {
     slug: "orderId",
     title: "Bestellnummer",
-    head: "Nachname",
-    toText: (tn: TeilnehmerIn) => tn.orderId,
+    head: '"Bestellnummer"',
+    toText: (tn: TeilnehmerIn) => `"${tn.orderId}"`,
   },
   tribe: {
     slug: "tribe",
     title: "Stamm",
-    head: "Stamm",
-    toText: (tn: TeilnehmerIn) => getTribeForNumber(Number(tn.tribe)).name,
+    head: '"Stamm"',
+    toText: (tn: TeilnehmerIn) => `"${getTribeForNumber(Number(tn.tribe)).name}"`,
   },
   role: {
     slug: "role",
     title: "Rolle",
-    head: "Rolle",
-    toText: (tn: TeilnehmerIn) => getRoleName(tn.role),
+    head: '"Rolle"',
+    toText: (tn: TeilnehmerIn) => `"${getRoleName(tn.role)}"`,
   },
   level: {
     slug: "level",
     title: "Stufe",
-    head: "Stufe",
-    toText: (tn: TeilnehmerIn) => getLevelWithNone(tn.level).singular,
+    head: '"Stufe"',
+    toText: (tn: TeilnehmerIn) => `"${getLevelWithNone(tn.level).singular}"`,
   },
   state: {
     slug: "state",
     title: "Status",
-    head: "Status",
-    toText: (tn: TeilnehmerIn) => getState(tn.state).name,
+    head: '"Status"',
+    toText: (tn: TeilnehmerIn) => `"${getState(tn.state).name}"`,
   },
   lateRegistration: {
     slug: "lateRegistration",
     title: "Buchungsart",
-    head: "Buchungsart",
+    head: '"Buchungsart"',
     toText: (tn: TeilnehmerIn) => {
       if (tn.lateRegistration !== undefined) {
-        return tn.lateRegistration ? "Nachmeldung" : "Normal"
+        return tn.lateRegistration ? '"Nachmeldung"' : '"Normal"'
       } else {
-        return "Normal"
+        return '"Normal"'
       }
     }
   },
   cancelDate: {
     slug: "cancelDate",
     title: "Stornierungsdatum",
-    head: "Stornierungsdatum",
+    head: '"Stornierungsdatum"',
     toText: (tn: TeilnehmerIn) => {
       if (tn.cancelledAt !== undefined) {
-        return moment(tn.cancelledAt).tz("Europe/Berlin").format("DD.MM.YYYY")
+        return `"${moment(tn.cancelledAt).tz("Europe/Berlin").format("DD.MM.YYYY")}"`
       } else {
-        return ""
+        return '""'
       }
     },
   },
   birthDate: {
     slug: "birthDate",
     title: "Geburtstag",
-    head: "Geburtstag",
-    toText: (tn: TeilnehmerIn) => moment(tn.birthDate).tz("Europe/Berlin").format("DD.MM.YYYY"),
+    head: '"Geburtstag"',
+    toText: (tn: TeilnehmerIn) => `"${moment(tn.birthDate).tz("Europe/Berlin").format("DD.MM.YYYY")}"`,
   },
   gender: {
     slug: "gender",
     title: "Geschlecht",
-    head: "Geschlecht",
-    toText: (tn: TeilnehmerIn) => getGender(tn.gender).name,
+    head: '"Geschlecht"',
+    toText: (tn: TeilnehmerIn) => `"${getGender(tn.gender).name}"`,
   },
   email: {
     slug: "email",
     title: "E-Mail",
-    head: "E-Mail",
-    toText: (tn: TeilnehmerIn) => tn.email,
+    head: '"E-Mail"',
+    toText: (tn: TeilnehmerIn) => `"${tn.email}"`,
   },
   phoneNumber: {
     slug: "phoneNumber",
     title: "Telefonnummer",
-    head: "Telefonnummer",
-    toText: (tn: TeilnehmerIn) => tn.phoneNumber,
+    head: '"Telefonnummer"',
+    toText: (tn: TeilnehmerIn) => `"${tn.phoneNumber || ""}"`,
   },
   address: {
     slug: "address",
     title: "Adresse",
-    head: "Straße;Postleitzahl;Ort",
+    head: '"Straße";"Postleitzahl";"Ort"',
     toText: (tn: TeilnehmerIn) => {
-      return `${tn.address.street};${tn.address.zipCode};${tn.address.city}`
+      return `"${tn.address.street}";"${tn.address.zipCode}";"${tn.address.city}"`
     }
   },
   eatingBehaviour: {
     slug: "eatingBehaviour",
     title: "Essverhalten",
-    head: "Essverhalten",
-    toText: (tn: TeilnehmerIn) => getEatingBehaviour(tn.food.eatingBehaviour).name,
+    head: '"Essverhalten"',
+    toText: (tn: TeilnehmerIn) => `"${getEatingBehaviour(tn.food.eatingBehaviour).name}"`,
   },
   intolerances: {
     slug: "intolerances",
     title: "Unverträglichkeiten",
-    head: "Unverträglichkeiten",
-    toText: (tn: TeilnehmerIn) => tn.food.intolerances || "",
+    head: '"Unverträglichkeiten"',
+    toText: (tn: TeilnehmerIn) => `"${tn.food.intolerances || ""}"`,
   },
   tetanus: {
     slug: "tetanus",
     title: "Tetanus-Impfung",
-    head: "Tetanus-Impfung",
-    toText: (tn: TeilnehmerIn) => tn.vaccinations.tetanus,
+    head: '"Tetanus-Impfung"',
+    toText: (tn: TeilnehmerIn) => tn.vaccinations.tetanus ? '"Ja"' : '"Nein"',
   },
   covid: {
     slug: "covid",
     title: "Corona-Impfung",
-    head: "Corona-Impfung",
-    toText: (tn: TeilnehmerIn) => getCovidVaccinationState(tn.vaccinations.covid).name,
+    head: '"Corona-Impfung"',
+    toText: (tn: TeilnehmerIn) => `"${getCovidVaccinationState(tn.vaccinations.covid).name}"`,
   },
   fsme: {
     slug: "fsme",
     title: "FSME-Impfung",
-    head: "FSME-Impfung",
-    toText: (tn: TeilnehmerIn) => tn.vaccinations.fsme,
+    head: '"FSME-Impfung"',
+    toText: (tn: TeilnehmerIn) => tn.vaccinations.fsme ? '"Ja"' : '"Nein"',
   },
   diseases: {
     slug: "diseases",
     title: "Krankheiten",
-    head: "Krankheiten",
-    toText: (tn: TeilnehmerIn) => tn.diseases || "",
+    head: '"Krankheiten"',
+    toText: (tn: TeilnehmerIn) => `"${tn.diseases || ""}"`,
   },
   healthInsurance: {
     slug: "healthInsurance",
     title: "Krankenversicherung",
-    head: "Krankenversicherung",
-    toText: (tn: TeilnehmerIn) => getInsuranceType(tn.healthInsurance).name,
+    head: '"Krankenversicherung"',
+    toText: (tn: TeilnehmerIn) => `"${getInsuranceType(tn.healthInsurance).name}"`,
   },
   swimmer: {
     slug: "swimmer",
     title: "Schwimmer:in",
-    head: "Schwimmer:in",
-    toText: (tn: TeilnehmerIn) => tn.swimmer ? "Ja" : "Nein",
+    head: '"Schwimmer:in"',
+    toText: (tn: TeilnehmerIn) => tn.swimmer ? '"Ja"' : '"Nein"',
   },
   firstContact: {
     slug: "firstContact",
     title: "1. Notfallkontakt",
-    head: "Notfallkontakt;Notfallnummer",
+    head: '"Notfallkontakt"',
     toText: (tn: TeilnehmerIn) => {
       if (tn.legalGuardian !== undefined) {
-        return `${tn.legalGuardian.name || ""}: ${tn.legalGuardian.phoneNumber || ""}`
+        return `"${tn.legalGuardian.name || ""}: ${tn.legalGuardian.phoneNumber || ""}"`
       }
-      if (tn.contacts !== undefined) {
-        return `${tn.contacts[0].name}: ${tn.contacts[0].phoneNumber}`
+      if (tn.contacts !== undefined && tn.contacts[0] !== undefined) {
+        return `"${tn.contacts[0].name || ""}: ${tn.contacts[0].phoneNumber || ""}"`
       }
     },
   },
   presencePerDay: {
     slug: "presencePerDay",
     title: "Anwesenheit per Tag",
-    head: "04.;05.;06.;07.;08.;09.;10.;11.",
+    head: '"04.";"05.";"06.";"07.";"08.";"09.";"10.";"11."',
     toText: (tn: TeilnehmerIn) => {
-      return Object.values(tn.presence).map((present) => present ? "Ja" : "Nein").join(";")
+      return Object.values(tn.presence).map((present) => present ? '"Ja"' : '"Nein"').join(";")
     },
   },
   presence: {
     slug: "presence",
     title: "Anwesenheit in Tagen",
-    head: "Anwesenheit (Tage)",
+    head: '"Anwesenheit (Tage)"',
     toText: (tn: TeilnehmerIn) => {
-      return Object.values(tn.presence).filter((present) => present).length
+      return `"${Object.values(tn.presence).filter((present) => present).length}"`
     },
   },
   comments: {
     slug: "comments",
     title: "Anmerkungen",
-    head: "Anmerkungen",
-    toText: (tn: TeilnehmerIn) => tn.comments || "",
+    head: '"Anmerkungen"',
+    toText: (tn: TeilnehmerIn) => `"${tn.comments || ""}"`,
   },
   juleica: {
     slug: "juleica",
     title: "Juleica",
-    head: "Juleica;Juleica Ablauf",
+    head: '"Juleica";"Juleica Ablauf"',
     toText: (tn: TeilnehmerIn) => {
       const info = [tn.juleica?.number || ""]
       if (tn.juleica?.terminates !== undefined) {
@@ -204,58 +204,58 @@ const Fields = {
       } else {
         info.push("")
       }
-      return info.join(";")
+      return info.map(elem => `"${elem}"`).join(";")
     },
   },
   clearance: {
     slug: "clearance",
     title: "Unbedenklichkeitsbescheinigung",
-    head: "UB-ID;NaMi",
+    head: '"UB-ID";""NaMi"',
     toText: (tn: TeilnehmerIn) => {
-      const info = [tn.clearance?.idNumber ?? ""]
+      const info = [tn.clearance?.idNumber || ""]
       if (tn.clearance?.nami !== undefined) {
         info.push(tn.clearance.nami ? "In NaMi" : "")
       } else {
         info.push("")
       }
-      return info.join(";")
+      return info.map(elem => `"${elem}"`).join(";")
     }
   },
   course2d2e: {
     slug: "course2d2e",
     title: "2d2e-Schulung",
-    head: "2d2e-Schulung",
+    head: '"2d2e-Schulung"',
     toText: (tn: TeilnehmerIn) => {
       if (tn.course !== undefined) {
-        return moment(tn.course).tz("Europe/Berlin").format("DD.MM.YYYY")
+        return `"${moment(tn.course).tz("Europe/Berlin").format("DD.MM.YYYY")}"`
       } else {
-        return ""
+        return '""'
       }
     },
   },
   registration: {
     slug: "registration",
     title: "Anmeldung",
-    head: "Anmeldung",
-    toText: (tn: TeilnehmerIn) => tn.receivedRegistration ? "Ja" : "Nein",
+    head: '"Anmeldung"',
+    toText: (tn: TeilnehmerIn) => tn.receivedRegistration ? '"Ja"' : '"Nein"',
   },
   photoPermission: {
     slug: "photoPermission",
     title: "Fotoerlaubnis",
-    head: "Fotoerlaubnis",
-    toText: (tn: TeilnehmerIn) => getPhotopermissionState(tn.receivedPhotoPermission).name,
+    head: '"Fotoerlaubnis"',
+    toText: (tn: TeilnehmerIn) => `"${getPhotopermissionState(tn.receivedPhotoPermission).name}"`,
   },
   leaderInfo: {
     slug: "leaderInfo",
     title: "Leitendeninfos",
-    head: "Leitendeninfos",
-    toText: (tn: TeilnehmerIn) => tn.receivedLeaderInfo ? "Ja" : "Nein",
+    head: '"Leitendeninfos"',
+    toText: (tn: TeilnehmerIn) => tn.receivedLeaderInfo ? '"Ja"' : '"Nein"',
   },
 }
 
 const generateCSVLine = (tn: TeilnehmerIn, fields: string[]): string => {
   // @ts-ignore
-  return fields.map((field) => `"${Fields[field].toText(tn)}"`).join(";")
+  return fields.map((field) => Fields[field].toText(tn)).join(";")
 }
 
 const generateCSV = (tns: TeilnehmerIn[], fields: string[]): string => {
